07746efee3a01ac9efe2a19da431c1877e175cee
RogerVeraL <105682754+RogerVeraL@users.noreply.github.com>
2022-08-29 21:51:31 -0500

    :+1:

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller3.televisores;
+
+/**
+ *
+ * @author Roger Vera
+ */
+public class Control {
+    TV tv;
+
+    public TV getTv() {
+        return tv;
+    }
+
+    public void setTv(TV tv) {
+        this.tv = tv;
+    }
+
+    public void turnOn(){
+        tv.turnOn();
+    }
+    public void turnOff(){
+        tv.turnOff();
+    }
+    public void canalUp(){
+        tv.canalUp();
+    }
+    public void canalDown(){
+        tv.canalDown();
+    }
+    public void volumenUp(){
+        tv.volumenUp();
+    }
+    public void volumenDown(){
+        tv.volumenDown();
+    }
+    public void setCanal(int canal){
+        tv.setCanal(canal);  
+    }
+        public void enlazar(TV tv){
+            this.tv=tv;
+            tv.setControl(this);
+                    
+        
+    }
+}
---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,114 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller3.televisores;
+
+/**
+ *
+ * @author Roger Vera
+ */
+public class TV {
+    Marca marca;
+    int canal,precio,volumen;
+    boolean estado;
+    Control control;
+
+    public TV(Marca marca, boolean estado) {
+        this.marca = marca;
+        this.estado = estado;
+        this.canal=1;
+        this.volumen=1;
+        this.precio=500;
+    }
+
+    public Marca getMarca() {
+        return marca;
+    }
+
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+
+    public int getCanal() {
+        return canal;
+    }
+
+    public void setCanal(int canal) {
+        if(0<=canal && canal<=120 && getEstado()==true){
+            this.canal = canal;
+        } 
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getVolumen() {
+        return volumen;
+    }
+
+    public void setVolumen(int volumen) {
+        this.volumen = volumen;
+    }
+
+    public boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
+    public Control getControl() {
+        return control;
+    }
+
+    public void setControl(Control control) {
+        this.control = control;
+    }
+    
+    int numTV;
+    public void setNumTV(int numTV){
+        this.numTV=numTV;
+    }
+    
+    public void turnOn(){
+        if(getEstado()==false){
+            setEstado(true);
+        }  
+    }
+    public void turnOff(){
+        if(getEstado()==true){
+            setEstado(false);
+        }  
+    }
+    
+    public void canalUp(){
+        if(0<=getCanal()&& getCanal()<120 && getEstado()==true){
+            setCanal(getCanal()+1);
+        }   
+    }
+    public void canalDown(){
+        if(0<getCanal()&& getCanal()<=120 && getEstado()==true){
+            setCanal(getCanal()-1);
+        }   
+    }
+    public void volumenUp(){
+        if(0<=getVolumen()&& getVolumen()<7 && getEstado()==true){
+            setVolumen(getVolumen()+1);
+        }
+    }
+    public void volumenDown(){
+        if(0<getVolumen()&& getVolumen()<=7 && getEstado()==true){
+            setVolumen(getVolumen()-1);
+        }
+    }
+    
+    
+}
